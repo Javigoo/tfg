19a1c1cd2433bf07cf700041b46c0c76b7cf8700
javigoo <javierroiggregorio@gmail.com>
2021-03-25 16:16:43 +0100

    speedtest at deploy

---
+++ images/monitor/bandwidth.py
@@ -0,0 +1,11 @@
+import speedtest
+
+if __name__ == "__main__":    
+    s = speedtest.Speedtest()
+    rx = s.download()
+    tx = s.upload()
+
+    with open("bandwidth.txt", 'w') as f:
+        f.write("rx: {}, tx: {}\n", rx, tx)
+
+    #db.configuration.update({}, {$set: {"scaling.max_network_tx":0, "scaling.max_network_rx":0} })
