577408b435691bd1183feccacf9e1820c8c86f07
javigoo <javierroiggregorio@gmail.com>
2021-04-10 15:46:44 +0200

    bandwidth OK

--- images/monitor/bandwidth.py
+++ images/monitor/bandwidth.py
@@ -8,14 +8,5 @@ if __name__ == "__main__":
     s = speedtest.Speedtest()
     rx = s.download()
     tx = s.upload()
-
-    #with open("bandwidth.txt", 'w') as f:
-        #f.write("rx: "+str(rx)+", tx: "+str(tx)+"\n")
-    #db.configuration.update({}, {$set: {"scaling.max_network_tx":0, "scaling.max_network_rx":0} })
     
-    requests.put(CONFIG_URL, data=json.dumps({},
-        {
-            '$set': {
-            "scaling.max_network_tx":rx, "scaling.max_network_rx":tx
-            } 
-        }))
+    requests.put(CONFIG_URL, data=json.dumps([{}, {"$set": {"scaling.max_network_tx":0, "scaling.max_network_rx":0} }]))
