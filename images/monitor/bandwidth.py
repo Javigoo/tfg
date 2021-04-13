493307c0e7ce19220f3dbd4849f4a1fc9e9c5f5a
javigoo <javierroiggregorio@gmail.com>
2021-04-13 14:09:02 +0200

    debug monitor

--- images/monitor/bandwidth.py
+++ images/monitor/bandwidth.py
@@ -1,12 +1,25 @@
 import json
 import requests
 import speedtest
+import traceback
+import logging
 
 CONFIG_URL = 'http://mongoapi:8000/query'
 
-if __name__ == "__main__":    
-    s = speedtest.Speedtest()
-    rx = s.download()
-    tx = s.upload()
+if __name__ == "__main__":
+    try:
+        print("OK")
+        s = speedtest.Speedtest()
+        rx = s.download()
+        tx = s.upload()
+        
+        requests.put(CONFIG_URL, data=json.dumps({"tx":tx, "rx":rx}))
+    except Exception as e:
+        print("No OK")
+        logging.error(traceback.format_exc())
+
+        with open("log", 'w') as f:
+            f.write(e)
+        
+
     
-    requests.put(CONFIG_URL, data=json.dumps({"tx":tx, "rx":rx}))
